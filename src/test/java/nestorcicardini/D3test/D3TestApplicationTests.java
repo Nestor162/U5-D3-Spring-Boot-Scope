b27ab146e79db18584d2f398ba4a25267188e3ed
Nestor162 <nestorcicardini@gmail.com>
2023-05-31 14:26:46 +0200

    Caricati file iniziali

---
+++ src/test/java/nestorcicardini/D3test/D3TestApplicationTests.java
@@ -0,0 +1,13 @@
+package nestorcicardini.D3test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D3TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
