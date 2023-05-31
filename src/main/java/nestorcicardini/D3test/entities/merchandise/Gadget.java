8d7ffb1260bd91f7ab45047b53a7d76864dd0702
Nestor162 <nestorcicardini@gmail.com>
2023-05-31 16:05:46 +0200

    Classi Gadgets

--- src/main/java/nestorcicardini/D3test/entities/merchandise/Gadget.java
+++ src/main/java/nestorcicardini/D3test/entities/merchandise/Gadget.java
@@ -1,15 +1,26 @@
 package nestorcicardini.D3test.entities.merchandise;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 import nestorcicardini.D3test.entities.Product;
 
 @Setter
 @Getter
 @NoArgsConstructor
-@ToString
+@AllArgsConstructor
 public class Gadget extends Product {
-	private String type;
+	private gadgetType type;
+
+	public enum gadgetType {
+		shirt, mug, other
+	}
+
+	@Override
+	public String toString() {
+		return "Gadget [type=" + type + ", name=" + name + ", price=" + price
+				+ "]";
+	}
+
 }
---
+++ src/main/java/nestorcicardini/D3test/entities/merchandise/Mug.java
@@ -0,0 +1,11 @@
+package nestorcicardini.D3test.entities.merchandise;
+
+public class Mug extends Gadget {
+	public Mug(gadgetType type) {
+		super(type);
+		this.name = "White Mug";
+		this.price = 15.30;
+
+	}
+
+}
