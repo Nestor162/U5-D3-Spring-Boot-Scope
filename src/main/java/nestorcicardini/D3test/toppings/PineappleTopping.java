80b2d0c7641c9ef6d93167101ee27c773e02f05f
Nestor162 <nestorcicardini@gmail.com>
2023-05-31 15:22:57 +0200

    Corretto nome classe

--- src/main/java/nestorcicardini/D3test/toppings/PineappleTopping.java
+++ src/main/java/nestorcicardini/D3test/toppings/PineappleTopping.java
@@ -2,8 +2,8 @@ package nestorcicardini.D3test.toppings;
 
 import nestorcicardini.D3test.entities.Consummation;
 
-public class PieneappleTopping extends ToppingsDecorator {
-	public PieneappleTopping(Consummation c) {
+public class PineappleTopping extends ToppingsDecorator {
+	public PineappleTopping(Consummation c) {
 		super(c);
 		this.name = "Pineapple";
 		this.price = 0.79;
